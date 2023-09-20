b3ec2c65a946475ac86e0169d9f0310b9fdd5f49
Divya <divya.2226cseai1130@kiet.edu>
2023-09-20 19:07:52 +0530

    First commit

---
+++ learning javascript/Day 1/index.js
@@ -0,0 +1,53 @@
+// console.log('namaskar dunia');
+
+// let b = 'love';
+// console.log(b) ;
+
+
+
+// let age = 47;
+// let status = (age >= 18) ? 'yes vote' : 'no vote';
+// console.log(status);
+
+
+
+// let marks = 89;
+
+// if(marks >= 98){
+//     console.log('A');
+// }
+// else if(marks >= 79){
+//     console.log('B');
+// }
+// else if(marks >= 60){
+//     console.log('C');
+// }
+// else {
+//     console.log('D');
+// }
+
+
+
+// for(let i = 1;i<=5; i++){
+//     console.log('Divya');
+// }
+
+
+
+
+// let x = 1;
+
+// while(x <= 3) {
+//     console.log(x);
+//     x++;
+// }
+
+
+
+// let y = 1;
+
+// do{
+//     console.log('y');
+//     y++;
+// }
+// while(y < 10);
